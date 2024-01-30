d77b7f9311ee7cff2137abd23eb58b82b56d5049
Portarriaux <jhonatacaetano14@hotmail.com>
2024-01-30 15:39:36 -0300

    Refatora estrutura do footer.

--- foodexplorer/src/components/Footer/index.jsx
+++ foodexplorer/src/components/Footer/index.jsx
@@ -4,9 +4,11 @@ import logoFooter from "../../assets/footer.png.png";
 export function Footer() {
   return (
     <Container>
-      <img src={logoFooter} alt="Logo" />
-      <h2>food explorer</h2>
-      <p>© 2023 - Todos os direitos reservados.</p>
+      <div className="section-footer">
+        <img src={logoFooter} alt="Logo" />
+        <h2>food explorer</h2>
+        <p>© 2023 - Todos os direitos reservados.</p>
+      </div>
     </Container>
   );
 }
--- foodexplorer/src/components/Footer/styles.js
+++ foodexplorer/src/components/Footer/styles.js
@@ -1,35 +1,36 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    width: 42.8rem;
-    height: 7.7rem;
+  width: 42.8rem;
+  margin-top: 8rem;
 
+  .section-footer {
+    height: 7.7rem;
     display: flex;
     align-items: center;
-    padding-left: 2rem;
+    gap: 4px;
 
     position: fixed;
     bottom: 0;
-
-    gap: 4px;
     flex-shrink: 0;
 
     background-color: ${({ theme }) => theme.COLORS.BACKGROUND_700};
 
-  h2 {
-    font-family: "Roboto";
-    font-style: normal;
-    line-height: normal;
-    font-size: 1.6rem;
-    font-weight: 700;
-    padding-left: 8px;
-    flex-shrink: 0;
+    h2 {
+      font-family: "Roboto";
+      font-style: normal;
+      line-height: normal;
+      font-size: 1.6rem;
+      font-weight: 700;
+      padding-left: 8px;
+      flex-shrink: 0;
 
-    color: ${({ theme }) => theme.COLORS.GRAY_400};
-  }
+      color: ${({ theme }) => theme.COLORS.GRAY_400};
+    }
 
-  p {
-    margin-left: 15px;
-    flex-shrink: 0;
+    p {
+      margin-left: 15px;
+      flex-shrink: 0;
+    }
   }
 `;
